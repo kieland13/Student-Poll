640bacde4a1d7b017641e222ffa41395f79931dc
Nick Kieland <nkieland@icloud.com>
2020-12-18 20:12:50 -0600

    first commit

---
+++ StudentPoll/StudentPollForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StudentPoll
+{
+   partial class StudentPollForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.displayTextBox = new System.Windows.Forms.TextBox();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.Enabled = false;
+            this.displayButton.Location = new System.Drawing.Point(107, 133);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(118, 26);
+            this.displayButton.TabIndex = 3;
+            this.displayButton.Text = "View Survey Results";
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(66, 86);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 23);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter an integer result (1 - 10) and press Enter:";
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.Location = new System.Drawing.Point(66, 174);
+            this.displayTextBox.Multiline = true;
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.ReadOnly = true;
+            this.displayTextBox.Size = new System.Drawing.Size(205, 229);
+            this.displayTextBox.TabIndex = 5;
+            // 
+            // doneButton
+            // 
+            this.doneButton.Location = new System.Drawing.Point(195, 86);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Size = new System.Drawing.Size(75, 23);
+            this.doneButton.TabIndex = 6;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // StudentPollForm
+            // 
+            this.ClientSize = new System.Drawing.Size(345, 438);
+            this.Controls.Add(this.doneButton);
+            this.Controls.Add(this.displayTextBox);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "StudentPollForm";
+            this.Text = "Student Poll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button displayButton;
+      private System.Windows.Forms.TextBox inputTextBox;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox displayTextBox;
+        private System.Windows.Forms.Button doneButton;
+    }
+
+}
---
+++ StudentPoll/StudentPollForm.cs
@@ -0,0 +1,129 @@
+﻿// Ex. 17.8: StudentPoll.cs
+// Allow student to take a survey
+// and view the results in a TextBox
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Xsl;
+using System.Globalization;
+
+namespace StudentPoll
+{
+   public partial class StudentPollForm : Form
+   {
+
+      //initiliaze StreamWriter, StreamWriter and int variables
+      StreamWriter writer = null;
+      StreamReader input;
+      int number;
+      
+      // parameterless constructor
+      public StudentPollForm()
+      {
+         InitializeComponent();
+
+         //set the app to write everything to a text document called "numbers.txt"
+         writer = new StreamWriter("numbers.txt");
+         //TODO ***************************
+         //Create the StreamWriter object
+
+      } // end constructor
+
+      private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+      {
+         //checks to see if user presses the user button
+         if (e.KeyCode == Keys.Enter)
+         {
+            if (e.KeyCode != Keys.Enter)
+                {
+                    //if user does not press the enter button, show this message
+                    MessageBox.Show("Must press the enter button to submit.", "Press enter",
+                        MessageBoxButtons.OK);
+                }
+            //TODO ************************
+            //Add exception handling
+
+            if (String.IsNullOrEmpty(inputTextBox.Text))
+            {
+               //if the user tries to submit blank text, show this message
+               MessageBox.Show("Please fill in the TextBox.");
+            } // end if
+            else
+            {
+               //takes user input and translates it to a number
+               number = int.Parse(inputTextBox.Text);
+
+                    //if user types in a number not between 1-10, show this message
+                    if (number < 1 || number > 10)
+                    {
+                        MessageBox.Show("The number has to be between 1 and 10", "Wrong number choice",
+                        MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        //otherwise, take the user number and print it to the "numbers.txt" document
+                        writer.Write(number + " ");
+                    }    
+               //TODO ******************************
+               //  Validate that the number is 1 - 10. When valid, use method Write to put the value into the file
+
+               
+            } // end else
+
+            //clear the input text box
+            inputTextBox.Clear();
+         } // end if
+      }
+
+      private void doneButton_Click(object sender, EventArgs e)
+      {
+         //makes it so that nothing else can be inputted to the "numbers.txt" document
+         writer.Close();
+            //TODO *********************************
+            //Change properties such that the display button can be used and this button can't be used. The textbox should be read-only
+
+            //enables the display button and display Text box, disables the done button
+            displayButton.Enabled = true;
+            doneButton.Enabled = false;
+            displayTextBox.ReadOnly = true;
+      }
+
+      private void displayButton_Click(object sender, EventArgs e)
+      {
+         //reads the data from the "numbers.txt" document
+         input = new StreamReader("numbers.txt");
+
+         //reads what the user typed in
+         string inputString = input.ReadToEnd();
+         string[] stringArray;
+         int[] frequency = new int[11];
+
+            //TODO ******************
+            //makes every "word" in the document and adds them to indexes of the array
+            stringArray = inputString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int x = 0; x < stringArray.Length; x++)
+            {
+                int anotherInteger = Convert.ToInt32(stringArray[x]);
+                ++frequency[anotherInteger];
+            }
+         //Split the read data into the array. Populate another integer array with the values from the string array converted
+         //   to integers. Update the frequency array to include a count for each of the responses. Refer to Figure 8.8
+
+        
+         //clears the display text box and prints off the headers
+         displayTextBox.Clear();
+         displayTextBox.AppendText("Rating\tFrequency\n");
+
+            //TODO *******************
+            //Write out the ratings and frequencies
+            //prints off the numbers for which the user could have typed in
+            //and prints off how many each rating was typed in
+            for (var ratings = 1; ratings < frequency.Length; ++ratings)
+            {
+                displayTextBox.AppendText(("\r\n" + ratings + "\t                " + frequency[ratings]));
+                //displayTextBox.AppendText(String.Format("\r\n{0,10}{1,10}", ratings, frequency[ratings]));
+            }
+        } // end method displayButton_Click
+   } // end class StudentPollForm
+} // end namespace StudentPoll
+
